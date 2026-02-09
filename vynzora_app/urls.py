5a72806291495a9a223838afd835b21ead76c7db
abhijith-vynzora <abhijithp.vynzora@gmail.com>
2026-02-09 16:31:34 +0530

    removed imports from urls too

--- vynzora_app/urls.py
+++ vynzora_app/urls.py
@@ -118,7 +118,6 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
-from vynzora_app.views import create_admin  # Import the view
 
 urlpatterns = [
     # --- Public Pages ---
